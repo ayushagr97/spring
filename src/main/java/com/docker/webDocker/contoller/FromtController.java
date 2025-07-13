5592b8c7a0e0d32030da1767060252bed0caaf59
Ayush Agrawal <ayushagrawal@Ayushs-MacBook-Air.local>
2025-07-13 15:07:32 +0530

    docker image

---
+++ src/main/java/com/docker/webDocker/contoller/FromtController.java
@@ -0,0 +1,16 @@
+package com.docker.webDocker.contoller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello")
+public class FromtController {
+
+    @GetMapping
+    public String getWeb(){
+        return "hii";
+    }
+
+}
---
+++ src/test/java/com/docker/webDocker/WebDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.webDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
